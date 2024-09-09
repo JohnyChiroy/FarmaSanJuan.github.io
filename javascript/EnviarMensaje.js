48e2e497125b6c1ccdded1f48eafb1c13743c118
JohnyChiroy <91798432+JohnyChiroy@users.noreply.github.com>
2024-09-09 12:12:38 -0600

    Cambio formulario, mensaje y estilo.

--- javascript/EnviarMensaje.js
+++ javascript/EnviarMensaje.js
@@ -1,16 +1,15 @@
 document.getElementById('FormularioWhatsApp').addEventListener('submit', function(e) {
-    e.preventDefault(); // Evitar el comportamiento por defecto del formulario
+    e.preventDefault(); // Evitar el comportamiento default del formulario
 
-    // Capturar los valores del formulario
+    //Variables
     var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
     var mensaje = document.getElementById('mensaje').value;
 
-    // Número de teléfono con código de país (sin +), ej: 521XXXXXXXXXX para México
-    var NumeroTelefono = '+50254195742'; // Reemplazar con el número de WhatsApp destino
+    // Número de teléfono 
+    var NumeroTelefono = '+50254195742'; 
 
     // Crear el mensaje que se enviará
-    var MensajeWhatsApp = `Hola, soy ${nombre}. Mi email es ${email}. Mensaje: ${mensaje}`;
+    var MensajeWhatsApp = `Hola, soy ${nombre}. Mensaje: ${mensaje}`;
 
     // Generar la URL de WhatsApp
     var whatsappURL = `https://api.whatsapp.com/send?phone=${NumeroTelefono}&text=${encodeURIComponent(MensajeWhatsApp)}`;
